c052489293fe654184f830b3a692c0f649edde72
ncwarren1 <90632261+ncwarren1@users.noreply.github.com>
2023-02-05 12:48:06 -0500

    adding files for env setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+import requests
+
+
+class FlaskTest(unittest.TestCase):
+
+    def test_index(self):
+        response = requests.get("http://127.0.0.1:5000/")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+
+
+if __name__ == " __main__":
+    unittest.main()
