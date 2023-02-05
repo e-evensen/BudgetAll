1c8229401397c13d73714862a67a7e410096a86e
ncwarren1 <90632261+ncwarren1@users.noreply.github.com>
2023-02-05 13:00:57 -0500

    updated files

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def main():
-    return "Welcome!"
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>Use this site for all your budgeting needs.</h2>' in response.text, True)
 
 
 if __name__ == " __main__":
