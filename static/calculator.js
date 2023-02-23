39b309db5ae0c70f48077dc529250a28534ac4b3
Elijah <elijah.evensen@gmail.com>
2023-02-23 08:15:46 -0500

    Got the calculator function working using an external script file

---
+++ static/calculator.js
@@ -0,0 +1,32 @@
+function calculate(cost, income) {
+
+    if (cost == "" || isNaN(cost) || cost < 1) {
+        //alert("Please enter a valid, positive number greater than one for your cost");
+        return "badCost";
+    }
+    else if (income == "" || isNaN(income) || income < 1) {
+        //alert("Please enter a valid, positive number greater than one for your income");
+        return "badIncome";
+    }
+    else {
+        return Math.ceil(cost / income);
+    }
+}
+
+function displayResult() {
+    const cost = document.forms["calcForm"]["cost"].value;
+    const income = document.forms["calcForm"]["income"].value;
+    const resultElement = document.getElementById("result");
+    
+    const result = calculate(cost, income);
+
+    if (result === "badCost") {
+        resultElement.innerHTML = "Please enter a valid, positive number greater than one for your cost";
+    }
+    else if (result === "badIncome") {
+        resultElement.innerHTML = "Please enter a valid, positive number greater than one for your income";
+    }
+    else {
+        resultElement.innerHTML = "It would take you roughly " + result + " days to save up $" + cost + ".";
+    }
+}
