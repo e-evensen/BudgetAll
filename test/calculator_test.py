eb8aa7aa77049a59fe3a7fb443a524f81b6e6aa1
ncwarren1 <nwarrennc@gmail.com>
2023-04-02 17:29:25 -0400

    I guess calc flask test is done?

--- test/calculator_test.py
+++ test/calculator_test.py
@@ -1,11 +1,24 @@
-from flask_testing import TestCase
-from main import create_app
-from database import db
-from models import User as User
-import bcrypt
-from forms import RegisterForm
 from base import BaseTestCase
 import unittest
+
+
+class TestCalculator(BaseTestCase):
+    def test_no_user(self):
+        response = self.client.get('/calculator')
+        assert response.status_code == 200
+        assert b'Enter how much you want to save up' in response.data
+
+    def test_logged_in_user(self):
+        with self.client:
+            self.client.post(
+                '/login', data=dict(
+                    email='testing@test.com',
+                    password='test_password'
+                ), follow_redirects=True
+            )
+        response = self.client.get('/calculator')
+        assert response.status_code == 200
+        assert b'Enter how much you want to save up' in response.data
 
 
 if __name__ == " __main__":
