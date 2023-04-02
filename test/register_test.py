4a8cdcd8d2a32780ae26bfd91e28c15a3b5b3515
ncwarren1 <90632261+ncwarren1@users.noreply.github.com>
2023-04-02 01:12:15 -0400

    Updated register tests

--- test/register_test.py
+++ test/register_test.py
@@ -1,54 +1,46 @@
-from flask_testing import TestCase
-from main import create_app
-from database import db
-from models import User as User
-import bcrypt
-from forms import RegisterForm
+
+from models import User
 from base import BaseTestCase
 import unittest
 
 
-class TestRegister(TestCase):
-    def create_app(self):
-        return create_app('test')
-
-    def setUp(self):
-        self.app = self.create_app()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        self.username = 'test_user'
-        self.email = 'test@example.com'
-        self.password = 'test_password'
-        self.hashed_password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
-        self.db = db
-        self.db.create_all()
-        self.user = User(username=self.username, email=self.email, password=self.hashed_password)
-        self.db.session.add(self.user)
-        self.db.session.commit()
-
-    def tearDown(self):
-        self.db.session.remove()
-        self.db.drop_all()
-        self.app_context.pop()
+class TestRegister(BaseTestCase):
+    def test_page(self):
+        response = self.client.get('/register')
+        assert response.status_code == 200
 
     # Tests a valid registration and that the user was stored in the database
     def test_user_registration(self):
-        with self.app.test_client() as client:
-            client.get("/logout", follow_redirects=True)
-            client.post("/register", data=dict(
-                    email="test@example.com",
-                    password="test_password"
-                ), follow_redirects=True)
-            user = User.query.filter_by(email="test@example.com").first()
-            assert user.id
-            assert user.email == "test@example.com"
+        with self.client:
+            response = self.client.post('/register', data=dict(
+                username='test',
+                email='test@test.com',
+                password='password123',
+                confirmPassword='password123'
+            ), follow_redirects=True)
+            user = User.query.filter_by(email='test@test.com').first()
+            assert b"Your balance is" in response.data
+            assert user.username == 'test'
 
-    def test_invalid_registration(self):
+    def test_invalid_email(self):
         with self.app.test_client() as client:
             response = client.post('/register', data=dict(
-                username=self.username,
-                email=self.email,
-                password=self.password,
+                username='test',
+                email='t',
+                password='password123',
+                confirmPassword='password12'
+            ), follow_redirects=True)
+
+            # check if registration was unsuccessful
+            assert response.status_code == 200
+            assert b'Not a valid email address.' in response.data
+
+    def test_confirm_password(self):
+        with self.app.test_client() as client:
+            response = client.post('/register', data=dict(
+                username='test',
+                email='test@test.com',
+                password='password123',
                 confirmPassword='password12'
             ), follow_redirects=True)
 
@@ -56,10 +48,36 @@ class TestRegister(TestCase):
             assert response.status_code == 200
             assert b'Passwords must match' in response.data
 
-    def test_register_link_on_home_page(self):
+    def test_no_username(self):
         with self.app.test_client() as client:
-            response = client.get('/')
-            assert b'<a href="/register">Register</a>' in response.data
+            response = client.post('/register', data=dict(
+                username='',
+                email='test@test.com',
+                password='password123',
+                confirmPassword='password123'
+            ), follow_redirects=True)
+
+            # check if registration was unsuccessful
+            assert response.status_code == 200
+            assert b'Field must be between 1 and 10 characters long.' in response.data
+
+    def test_same_email(self):
+        with self.app.test_client() as client:
+            client.post('/register', data=dict(
+                username='test',
+                email='test@test.com',
+                password='password123',
+                confirmPassword='password123'
+            ), follow_redirects=True)
+
+            response = client.post('/register', data=dict(
+                username='test1',
+                email='test@test.com',
+                password='password123',
+                confirmPassword='password123'
+            ), follow_redirects=True)
+            assert response.status_code == 200
+            assert b'Email already in use.' in response.data
 
 
 if __name__ == " __main__":
