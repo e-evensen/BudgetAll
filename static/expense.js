cb6e6c4ffd0eb72c59b6fb9c0721b6e71235fd28
cjuberaUNCC <cjubera@uncc.edu>
2023-04-03 18:25:27 -0400

    update html for view_expense

---
+++ static/expense.js
@@ -0,0 +1,7 @@
+
+  function addExpenseModal() {
+    var modal = document.getElementById("add-expense-modal");
+    if (modal.style.display === "none" || modal.style.display === "") {
+        modal.style.display = "block";
+    }
+}
