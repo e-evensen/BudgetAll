27380b7d3729f99ddae9e110809354b515b65e94
ncwarren1 <nwarrennc@gmail.com>
2023-05-01 12:18:27 -0400

    Completed purchase testing

---
+++ test/test_purchases.py
@@ -0,0 +1,83 @@
+from models import User, Purchase
+from base import BaseTestCase
+import unittest
+
+
+class TestPurchase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = User.query.filter_by(username="testy").first()
+
+    def test_page(self):
+        response = self.client.get('/purchases')
+        assert response.status_code == 200
+
+    def test_purchase_requires_login(self):
+        with self.client:
+            response = self.client.get('/purchases', follow_redirects=True)
+            assert response.status_code == 200
+            print(response.data)
+            assert b'Sign In' in response.data
+
+    def test_purchase_page_with_user(self):
+        with self.client:
+            self.client.post(
+                '/login', data=dict(
+                    email='testing@test.com',
+                    password='test_password'
+                ), follow_redirects=True
+            )
+
+            response = self.client.get('/purchases')
+            print(response.data)
+            assert response.status_code == 200
+            assert b'test purchase' in response.data
+            assert b'300' in response.data
+
+    def test_purchases(self):
+        with self.client:
+            self.client.post(
+                '/login', data=dict(
+                    email='testing@test.com',
+                    password='test_password'
+                ), follow_redirects=True
+            )
+
+            response = self.client.post(
+                '/add_purchases',
+                data=dict(product_name='test purchase',
+                          product_amount='10.99'
+                          ),
+                follow_redirects=True)
+            print(response.data)
+            assert response.status_code == 200
+
+            purchase = Purchase.query.filter_by(pur_name='test purchase').first()
+            assert purchase is not None
+            assert b'<td>$ 10.99</td>' in response.data
+
+            response = self.client.get('/purchases')
+            assert b'test purchase' in response.data
+
+    def test_amount_negative_input(self):
+        with self.client:
+            self.client.post(
+                '/login', data=dict(
+                    email='testing@test.com',
+                    password='test_password'
+                ), follow_redirects=True
+            )
+            response = self.client.post(
+                '/add_purchases',
+                data=dict(product_name='test purchase',
+                          product_amount='-10'
+                          ), follow_redirects=True
+            )
+            print(response.data)
+            assert b'$ -10.00' not in response.data
+            assert b'<td>$ 300.0</td>\n' in response.data
+
+
+if __name__ == " __main__":
+    unittest.main()
+
